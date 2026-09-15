f60035651ec7adfd963a2dfcdc22482af313413c
BhoneThit-31 <BhoneThit-31@users.noreply.github.com>
2026-09-15 23:07:17 +0630

    key hardcode upload

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    "https://zyscvhfwiynsbusfinxf.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5c2N2aGZ3aXluc2J1c2ZpbnhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0NzMxMjYsImV4cCI6MjEwNTA0OTEyNn0.ojiEZOiQ0BQheKxIa5eIV1mUbqrEc-E1BnKeT2QCcs4",
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,8 @@ export async function middleware(request: NextRequest) {
   let response = NextResponse.next({ request });
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    'https://zyscvhfwiynsbusfinxf.supabase.co',
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5c2N2aGZ3aXluc2J1c2ZpbnhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0NzMxMjYsImV4cCI6MjEwNTA0OTEyNn0.ojiEZOiQ0BQheKxIa5eIV1mUbqrEc-E1BnKeT2QCcs4",
     {
       cookies: {
         getAll() {
